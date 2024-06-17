ae053f129f0d4c45b38f36eff839319fe3d60ad4
sheepdotcom <magicsheepwizard3@gmail.com>
2024-06-17 17:37:12 -0400

    fixed music library

--- about.md
+++ about.md
@@ -4,4 +4,4 @@ A simple mod that aims to improve various menus related to songs!
 
 # Features
 
-- Re-adds the delete button but in even more menus.
+- Re-adds the delete button but in even more menus including the music library.
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 
 using namespace geode::prelude;
 
+void updateVariables(CustomSongWidget* levelInfoWidget, CustomSongWidget* musicLibraryWidget) { //I hate these kinds of work-arounds
+	auto downloaded = MusicDownloadManager::sharedState()->isSongDownloaded(musicLibraryWidget->m_customSongID);
+	if (levelInfoWidget->m_songs.contains(musicLibraryWidget->m_customSongID)) {
+		levelInfoWidget->m_songs.at(musicLibraryWidget->m_customSongID) = downloaded; //Manually editing this grrr
+	}
+}
+
 void updateButtons(CustomSongWidget* customSongWidget) {
-	geode::log::debug("{}", customSongWidget->m_songs.size());
+	if (customSongWidget->m_customSongID == 0 && !customSongWidget->m_isRobtopSong && customSongWidget->m_songs.size() == 0) return;
+	geode::log::debug("{}", customSongWidget->m_unkBool2);
 	auto undownloadedAsset = false;
 	if (customSongWidget->m_songs.size() > 0) {
 		for (auto song : customSongWidget->m_songs) {
@@ -29,12 +37,28 @@ void updateButtons(CustomSongWidget* customSongWidget) {
 	}
 	else {
 		geode::log::debug("all assets downloaded!");
-		customSongWidget->m_deleteBtn->setVisible(!customSongWidget->m_isRobtopSong); //If only robtop song then no delete for now :3
+		customSongWidget->m_deleteBtn->setVisible(!customSongWidget->m_isRobtopSong && customSongWidget->m_showDownloadBtn); //If only robtop song then no delete for now :3
 		customSongWidget->m_downloadBtn->setVisible(false);
-		if (customSongWidget->m_isMusicLibrary) {
-			customSongWidget->m_selectSongBtn->setPosition(ccp(-227.5f,-160.f));
-			customSongWidget->m_playbackBtn->setPosition(ccp(-187.5f,-160.f));
-			customSongWidget->m_deleteBtn->setPosition(ccp(-147.5f,-160.f));
+	}
+	if (customSongWidget->m_isMusicLibrary) {
+		geode::log::debug("isMusicLibrary");
+		customSongWidget->m_selectSongBtn->setPosition(ccp(-227.5f, -160.f));
+		customSongWidget->m_playbackBtn->setPosition(ccp(-187.5f, -160.f));
+		customSongWidget->m_deleteBtn->setPosition(ccp(-147.5f, -160.f));
+		customSongWidget->m_downloadBtn->setPosition(ccp(-147.5f, -160.f));
+
+		customSongWidget->m_songLabel->limitLabelWidth(135.f - (customSongWidget->m_songLabel->getPositionX() + 150), 0.5f, 0.01f);
+		customSongWidget->m_songIDLabel->limitLabelWidth(185.0f, 0.6f, 0.01f);
+		auto pos = customSongWidget->m_songLabel->getPositionX() + customSongWidget->m_songLabel->getScaledContentWidth() - 280.f;
+		customSongWidget->m_artistLabel->limitLabelWidth(-250 - pos, 0.5f, 0.01f);
+		customSongWidget->m_moreBtn->setContentSize(ccp(customSongWidget->m_artistLabel->getScaledContentWidth(), 16.f));
+		customSongWidget->m_artistLabel->setPosition(customSongWidget->m_moreBtn->getContentSize() / 2);
+		customSongWidget->m_moreBtn->setPositionX(pos + (customSongWidget->m_moreBtn->getScaledContentWidth() / 2));
+
+		if (auto levelInfo = static_cast<LevelInfoLayer*>(CCDirector::sharedDirector()->getRunningScene()->getChildByID("LevelInfoLayer"))) {
+			geode::log::debug("updating levelInfo");
+			updateVariables(levelInfo->m_songWidget, customSongWidget);
+			updateButtons(levelInfo->m_songWidget); //Should not be recursive because this one isnt musicLibrary widget
 		}
 	}
 }
